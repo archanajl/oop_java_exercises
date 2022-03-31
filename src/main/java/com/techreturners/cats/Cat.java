aaff9c1ec621c8a10a8e44a55b0972e1ef3d3b50
Archana Janarthanan <archanajl@gmail.com>
2022-03-31 20:29:25 +0100

    Created the DomesticCat class

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -2,11 +2,4 @@ package com.techreturners.cats;
 
 interface Cat {
 
-
-    boolean isAsleep();
-    void goToSleep();
-    void wakeUp();
-    String getSetting();
-    //int getAverageHeight();
-    //String eat();
 }
--- src/main/java/com/techreturners/cats/DomesticCat.java
+++ src/main/java/com/techreturners/cats/DomesticCat.java
@@ -6,27 +6,5 @@ package com.techreturners.cats;
      int height = 23;
      String domestic_wild = "domestic";
 
-     @Override
-    public boolean isAsleep(){
-         return is_asleep;
-    }
 
-     @Override
-     public void goToSleep() {
-         is_asleep = true;
-     }
-
-     @Override
-     public void wakeUp() {
-         is_asleep = false;
-     }
-
-     @Override
-     public String getSetting() {
-         return domestic_wild;
-     }
-
-     public int getHeight() {
-         return height;
-     }
  }
